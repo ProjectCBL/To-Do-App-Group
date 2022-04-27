41c226bf3f0abd1e8b52ed1ac9e711819970321c
Kelman <eleanor.kelman@capgemini.com>
2022-04-27 12:25:30 -0400

    Open and Close DB Connection

--- index.php
+++ index.php
@@ -3,41 +3,26 @@
     // Make sure to install PHP and place php.ini in php directory.
     // To launch the server in vscode -> CRTL+SHFT+B -> Select the task (if not launched automatically)
 
-    function connectToDB($servername, $username, $password, $dbname){
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully<br>";
-        return $conn;
-    } 
-    
-    function printResults($result){
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "tID: " . $row["tID"]. " - Title: \n" . $row["Title"]."<br>";
-            }
-        } else {
-            echo "0 results";
-        }
+    // DB information
+    $server = "localhost";
+    $username = "abcde";
+    $password = "password1";
+
+    // open connection to DB
+    $conn = new mysqli_connect($server, $username, $password);
+
+    if (!$conn) {
+      die("Connection to DB failed!");
+    }
+    else {
+      echo "Connected to DB!";
     }
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "todo";
+    //
+    // TODO: DO SOMETHING HERE!
+    //
 
-    $conn = connectToDB($servername, $username, $password, $dbname);
-
-    $sql = "SELECT * FROM TASKS";
-    $result = $conn->query($sql);
-
-    printResults($result);
-
-    $conn->close();
-
-    //phpinfo();
+    // close connection to DB
+    mysqli_close($conn);
     
 ?>

5fcb0098b6b34c527bf58a3242127135ca3fd616
ProjectCBL <projectxcbl@gmail.com>
2022-04-26 23:44:41 -0400

    Added php and launch instructions

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+    // Make sure to install PHP and place php.ini in php directory.
+    // To launch the server in vscode -> CRTL+SHFT+B -> Select the task (if not launched automatically)
+
     function connectToDB($servername, $username, $password, $dbname){
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);

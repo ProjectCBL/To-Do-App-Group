90160111fc0dce6e685333d8fc9ba09be6469767
ProjectCBL <projectxcbl@gmail.com>
2022-05-04 16:36:39 -0400

    Restructured to accomodate jquery

---
+++ JavaScript/login-actions.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $("#login").click(function(){
+
+        let username = $("#username").val();
+        let password = $("#pass").val();
+        
+        if (username.length == 0 || password.length == 0){
+            $("#invalid").html("Missing field, please correct...");
+        }
+        else{
+            
+            let dataParameters = `username=${username}&password=${password}`;
+
+            $.ajax({
+                type: "GET",
+                url: "../Services/login.php",
+                data: dataParameters,
+                cache: false,
+                success: (data)=>{
+                    console.log(data);
+                    (data.includes("Login Error!!")) ? 
+                        $("#invalid").html("Account details don't match...") : 
+                        $("#body").html(data);
+                }
+            });
+
+        }
+
+    });
+
+    $("#signup").click(function(event){
+
+        event.preventDefault();     // Keeps the page from freshing back to home/login screen
+
+        $.ajax({
+            type: "GET",
+            url: "../Services/signup_redirect.php",
+            success: (data)=>{$("#body").html(data);}
+        });
+
+    });
+
+});
+

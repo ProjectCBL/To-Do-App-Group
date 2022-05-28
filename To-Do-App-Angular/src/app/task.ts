0c569a4bf7f90fe3ec5163ba06e55f23f52c141b
ProjectCBL <projectxcbl@gmail.com>
2022-05-28 04:20:37 -0400

    Created classes for mapping purposes

---
+++ To-Do-App-Angular/src/app/task.ts
@@ -0,0 +1,11 @@
+export class Task{
+
+    public taskId:number = 0;
+    public title:string = "";
+    public description:string = "";
+    public accountId:number = 0;
+    public status:string = "";
+    public entryDate:string = "";
+    public dueDate:string = "";
+
+}
---
+++ To-Do-App-Angular/src/app/user.ts
@@ -0,0 +1,10 @@
+export class User{
+
+    public userId:number = 0;
+    public username:string = "";
+    public password:string = "";
+    public email:string = "";
+    public firstName:string = "";
+    public lastName:string = "";
+
+}

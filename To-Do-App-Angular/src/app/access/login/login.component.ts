0b0afe5805330aadfb5e9e3de63f0bdaa4f83738
ProjectCBL <projectxcbl@gmail.com>
2022-05-29 03:38:52 -0400

    Implemented login pag

--- To-Do-App-Angular/src/app/access/login/login.component.ts
+++ To-Do-App-Angular/src/app/access/login/login.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	@Output() switchToRegister = new EventEmitter();
 
-  ngOnInit(): void {
-  }
+	username:string = "";
+	password:string = "";
+
+	errorMessage:string = "";
+	showError:boolean = false;
+
+	constructor(private router:Router, private appService:AppService) { }
+
+	ngOnInit(): void {
+		localStorage.clear();
+	}
+
+	validateLogin(){
+
+		if(this.username == '' || this.password == ''){
+			this.showError = true;
+			this.errorMessage = "Missing field entry...";
+			return;
+		}
+
+		this.appService.validateLogin(this.username, this.password).subscribe((response:any)=>{
+
+			if(response != null){
+				localStorage.setItem("userId", response.userId);
+				localStorage.setItem("username", response.userName);
+				localStorage.setItem("firstname", response.firstName);
+				localStorage.setItem("lastName", response.lastName);
+				this.router.navigate(['/board']);
+				return;
+			}
+
+			this.showError = true;
+			this.errorMessage = "Incorrect login information...";
+			return;
+
+		},
+		(error)=>{
+			alert("It appears we can't log you in, please try again later...")
+		});
+
+	}
 
 }

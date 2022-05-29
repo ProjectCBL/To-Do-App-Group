200916af4cc224fc22c0cf0f953b9519e5c28c3c
ProjectCBL <projectxcbl@gmail.com>
2022-05-29 15:50:00 -0400

    Implemented update and add actions

--- To-Do-App-Angular/src/app/board/retrieve/retrieve.component.ts
+++ To-Do-App-Angular/src/app/board/retrieve/retrieve.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AppService } from 'src/app/app.service';
 import { Task } from 'src/app/task';
 
@@ -9,7 +9,8 @@ import { Task } from 'src/app/task';
 })
 export class RetrieveComponent implements OnInit {
 
-	@Input() view: string = "";
+	@Output() updateTask = new EventEmitter<Task>();
+	@Output() deleteTask = new EventEmitter<number>();
 
 	tasks: Task[] = [];
 	layout: Task[][] = [];
@@ -17,17 +18,17 @@ export class RetrieveComponent implements OnInit {
 	constructor(private appService: AppService) { }
 
 	ngOnInit(): void {
-		this.switchView();
+		this.switchView("all");
 	}
 
 	retrieveAllTasks() {
 
-		let userId = 1;
+		let userId = localStorage.getItem("userId") as unknown as number;
 
 		this.appService.getAllUserTask(userId).subscribe((response: any) => {
 			this.tasks = response.map((task: any) => { return this.mapToTask(task) });
 			this.layout = this.groupByThree(3, this.tasks);
-			console.log(this.layout);
+			localStorage.setItem("view", "all");
 		}, (error) => {
 
 		});
@@ -36,12 +37,12 @@ export class RetrieveComponent implements OnInit {
 
 	retrieveAllCompletedTasks(){
 
-		let userId = 1;
+		let userId = localStorage.getItem("userId") as unknown as number;
 
 		this.appService.getAllCompletedUserTask(userId).subscribe((response: any) => {
 			this.tasks = response.map((task: any) => { return this.mapToTask(task) });
 			this.layout = this.groupByThree(3, this.tasks);
-			console.log(this.layout);
+			localStorage.setItem("view", "done");
 		}, (error) => {
 
 		});
@@ -50,16 +51,80 @@ export class RetrieveComponent implements OnInit {
 
 	retrieveAllOngoingTasks(){
 
-		let userId = 1;
+		let userId = localStorage.getItem("userId") as unknown as number;
 
 		this.appService.getAllOngoingUserTask(userId).subscribe((response: any) => {
 			this.tasks = response.map((task: any) => { return this.mapToTask(task) });
 			this.layout = this.groupByThree(3, this.tasks);
-			console.log(this.layout);
+			localStorage.setItem("view", "ongoing");
 		}, (error) => {
 
 		});
 
+	}
+
+	addTaskToDB(task:Task){
+
+		let userId = localStorage.getItem("userId") as unknown as number;
+		let view = localStorage.getItem("view") as unknown as string;
+
+		task.status = (task.status == '') ? "Not-Started" : task.status;
+
+		this.appService.addNewTask(userId, task.title, task.description, task.status, task.dueDate).subscribe((response:any)=>{
+			console.log("Added Card");
+			console.log(task);
+			if(this.layout.length == 0){
+				this.switchView(view);
+			}else{
+
+				if(this.layout[this.layout.length-1].length != 3){
+					this.layout[this.layout.length-1].push(task);
+				}
+				else{
+					this.layout.push([task]);
+				}
+
+			}
+		}, (error)=>{
+			alert("Failed to update card, please try again later...");
+		});
+
+	}
+
+	updateTaskFromDB(task:Task){
+
+		let userId = localStorage.getItem("userId") as unknown as number;
+		let view = localStorage.getItem("view") as unknown as string;
+
+		console.log(task.taskId);
+
+		this.appService.updateTask(userId, task.taskId, task.title, task.description, task.status, task.dueDate, view).subscribe((response: any) => {
+			console.log("Updated Card");
+			for(let x = 0; x < this.layout.length; x++){
+				for(let y = 0; y < this.layout[x].length; y++){
+					if(this.layout[x][y].taskId == task.taskId){
+						this.layout[x][y] = task;
+						return;
+					}
+				}
+			}
+		},
+		(error)=>{
+			alert("Failed to update card, please try again later...");
+		});
+
+	}
+
+	deleteTaskFromDB(taskId:number){
+		let userId = localStorage.getItem("userId") as unknown as number;
+		let view = localStorage.getItem("view") as unknown as string;
+
+		this.appService.deleteTask(userId, taskId, view).subscribe((response:any)=>{
+			console.log('Deleted Task');
+			this.switchView(view);
+		}, (error) =>{
+			alert("Unable to delete task, please try again later...")
+		});
 	}
 
 	mapToTask(task: any): any {
@@ -83,8 +148,8 @@ export class RetrieveComponent implements OnInit {
 		return grouped;
 	}
 
-	switchView(){
-		switch(this.view){
+	switchView(view:string){
+		switch(view){
 			case "ongoing":
 				this.retrieveAllOngoingTasks();
 				break;
@@ -95,6 +160,15 @@ export class RetrieveComponent implements OnInit {
 				this.retrieveAllTasks();
 				break;
 		}
+	}
+
+	/*Bug: There appears to be bug or some sort of side effect that occurs prior to this method
+	being called. If an update has occurred before, the previous card will update to the current
+	card information.  In order to preserve the data we refresh the view.*/
+	emitUpdateTask(task:Task){
+		let view = localStorage.getItem("view") as unknown as string;
+		this.switchView(view);
+		this.updateTask.emit(task);
 	}
 
 }
